b6a9abd721c6c91182811f433ef5144b38e23a76
Raúl <raulguzmanguerrero@outlook.com>
2021-07-22 20:00:00 -0500

    Add main function

---
+++ calculator/calculator.cpp
@@ -0,0 +1,16 @@
+/* 
+    Angled brakets (<>) are used when including stuff from the standard library
+    Quotations ("") are used to include regular files
+*/
+#include <iostream>
+
+/*
+    Denote that the scope for the file is the standard library
+*/
+using namespace std;
+
+int main(){
+    cout << "This is a Cool Calculator Console Application \n";
+
+    return 0;
+}
