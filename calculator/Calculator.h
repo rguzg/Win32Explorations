90999b2807a04adaf02a0a4dae6631800deb3d1d
Raúl <raulguzmanguerrero@outlook.com>
2021-07-24 00:48:55 -0500

    Add Calculator::Calculate prototype

---
+++ calculator/Calculator.h
@@ -0,0 +1,12 @@
+/*
+    This is a header file. Functions are normally declared in header files 
+    and implemented in source files (*.cpp files)
+
+*/
+
+#pragma once
+
+class Calculator{
+    public:
+        double Calculate(double x, char operand, double y);
+};
